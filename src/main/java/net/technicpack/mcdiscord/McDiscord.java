4930c99938adf760bcc821944bbf5a4d5ae4d346
CannibalVox <sdbaynham@gmail.com>
2015-09-30 18:35:19 -0400

    Replace discord icon

--- src/main/java/net/technicpack/mcdiscord/McDiscord.java
+++ src/main/java/net/technicpack/mcdiscord/McDiscord.java
@@ -26,7 +26,7 @@ import org.apache.logging.log4j.Level;
 public class McDiscord extends DummyModContainer {
     public static final String MODID = "mcdiscord";
     public static final String NAME = "MC Discord";
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "1.0.1";
 
     public static CommonProxy proxy;
 
