1f2062408a4c40f6ce44c43e5a922d0278ddbcc9
CannibalVox <sdbaynham@gmail.com>
2015-10-14 19:48:59 -0400

    semver is a harsh mistress

--- src/main/java/net/technicpack/mcdiscord/McDiscord.java
+++ src/main/java/net/technicpack/mcdiscord/McDiscord.java
@@ -21,7 +21,7 @@ import org.apache.logging.log4j.Level;
 public class McDiscord extends DummyModContainer {
     public static final String MODID = "mcdiscord";
     public static final String NAME = "MC Discord";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "2.0.0";
 
     public static CommonProxy proxy;
 
